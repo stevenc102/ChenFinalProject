4f7249a09507f4516675300e21b0bf4b4bc5dfe4
1E10 Laptop <sample@bths.edu>
2023-05-24 12:30:30 -0400

    commit

--- src/Panel.java
+++ src/Panel.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class Panel extends JPanel implements Runnable {
     private final int SIZE = 16;
@@ -10,14 +11,18 @@ public class Panel extends JPanel implements Runnable {
     private final int MAX_SCREEN_ROW = 16;
     private final int SCREEN_WIDTH = TILE_SIZE * MAX_SCREEN_COL;
     private final int SCREEN_LENGTH = TILE_SIZE * MAX_SCREEN_ROW;
-
+    private Player player;
 
     private Thread gameThread;
     public Panel() {
+        player = new Player();
         this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_LENGTH));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
+        this.addKeyListener(player);
+        this.setFocusable(true);
         startGameThread();
+
     }
 
     public void startGameThread() {
@@ -28,14 +33,50 @@ public class Panel extends JPanel implements Runnable {
     public void run() {
         while(gameThread != null) {
             repaint();
+            updatePos();
         }
     }
+    public void updatePos() {
+        if (player.getUpPressed()) {
+            player.changeYPos(-1 * player.getPlayerSpeed());
+            try {
+                Thread.sleep(5);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        } else if (player.getDownPressed()) {
+            player.changeYPos(player.getPlayerSpeed());
+            try {
+                Thread.sleep(5);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        } else if (player.getRightPressed()) {
+            player.changeXPos(player.getPlayerSpeed());
+            try {
+                Thread.sleep(5);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        } else if (player.getLeftPressed()) {
+            player.changeXPos(-1 * player.getPlayerSpeed());
+            try {
+                Thread.sleep(5);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+//    public void paintComponent(Graphics g) {
+//        super.paintComponent(g);
+//        Graphics2D g2 = (Graphics2D) g;
+//        g2.setColor(Color.WHITE);
+//        g2.fillRect(player.getXPos(), player.getYPos() , 48, 48);
+//        g2.dispose();
+//    }
 
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        Graphics2D g2 = (Graphics2D) g;
-        g2.setColor(Color.WHITE);
-        g2.fillRect(100, 100 , 48, 48);
-        g2.dispose();
+    public void draw(Graphics2D g2) {
+        BufferedImage image = player.getImage();
+        g2.drawImage(image, player.getXPos(), player.getYPos(), 48, 48, null);
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,101 @@
-public class Player {
+import javax.imageio.ImageIO;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class Player implements KeyListener {
+    private int xPos, yPos;
+    private boolean upPressed, downPressed, leftPressed, rightPressed;
+
+    private BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
+    private int playerSpeed;
+
+    private BufferedImage img;
+    public Player()  {
+        playerSpeed = 2;
+        xPos = 100;
+        yPos = 100;
+        upPressed = false;
+        downPressed = false;
+        leftPressed = false;
+        rightPressed = false;
+        try {
+            getPlayerImage();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public int getXPos() {
+        return xPos;
+    }
+
+    public int getYPos() {
+        return yPos;
+    }
+    public int getPlayerSpeed() {
+        return playerSpeed;
+    }
+    public boolean getUpPressed() {
+        return upPressed;
+    }
+
+    public boolean getDownPressed() {
+        return downPressed;
+    }
+    public boolean getRightPressed() {
+        return rightPressed;
+    }
+    public boolean getLeftPressed() {
+        return leftPressed;
+    }
+
+    public void changeYPos(int a) {
+        yPos += a;
+    }
+    public void changeXPos(int a) {
+        xPos += a;
+    }
+
+    public void getPlayerImage() throws IOException {
+        img = ImageIO.read(getClass().getResourceAsStream("/Sprites/player.png"));
+    }
+
+    public BufferedImage getImage() {
+        return img;
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int temp = e.getKeyCode();
+        if (temp == KeyEvent.VK_W || temp == KeyEvent.VK_UP) {
+            upPressed = true;
+        } else if (temp == KeyEvent.VK_A || temp == KeyEvent.VK_LEFT) {
+            leftPressed = true;
+        } else if (temp == KeyEvent.VK_S || temp == KeyEvent.VK_DOWN) {
+            downPressed = true;
+        } else if (temp == KeyEvent.VK_D || temp == KeyEvent.VK_RIGHT) {
+            rightPressed = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int temp = e.getKeyCode();
+        if (temp == KeyEvent.VK_W || temp == KeyEvent.VK_UP) {
+            upPressed = false;
+        } else if (temp == KeyEvent.VK_A || temp == KeyEvent.VK_LEFT) {
+            leftPressed = false;
+        } else if (temp == KeyEvent.VK_S || temp == KeyEvent.VK_DOWN) {
+            downPressed = false;
+        } else if (temp == KeyEvent.VK_D || temp == KeyEvent.VK_RIGHT) {
+            rightPressed = false;
+        }
+    }
 }
